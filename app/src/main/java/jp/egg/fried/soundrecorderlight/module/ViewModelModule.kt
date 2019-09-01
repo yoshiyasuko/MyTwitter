32324f09d2a65551884fcf037fc7443344a39d85
Yoshiyasu <te108128@gmail.com>
2019-09-01 18:38:51 +0900

    add ViewModelModule

---
+++ app/src/main/java/jp/egg/fried/soundrecorderlight/module/ViewModelModule.kt
@@ -0,0 +1,14 @@
+package jp.egg.fried.soundrecorderlight.module
+
+import org.koin.dsl.module.module
+
+
+/**
+ * ViewModelModule
+ *
+ * Created by Yoshiyasu on 2019-09-01
+ */
+
+val viewModelModule = module {
+
+}
